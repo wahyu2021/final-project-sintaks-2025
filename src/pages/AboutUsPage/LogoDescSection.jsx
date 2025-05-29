f148f2c8b6997d3bf1ac0f1cf6f75ff5f59405ee
wahyu2021 <wahyuwn256@gmail.com>
2025-05-29 14:32:59 +0700

    feat: Add LogoDescSection component and integrate it into the router

---
+++ src/pages/AboutUsPage/LogoDescSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LogoDescSection() {
+  return (
+    <div>LogoDescSection</div>
+  )
+}
+
+export default LogoDescSection
--- src/router.jsx
+++ src/router.jsx
@@ -7,6 +7,7 @@ import ProductPage from "./pages/ProductPage/ProductPage";
 import AboutUsPage from "./pages/AboutUsPage/AboutUsPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import LogoDescSection from "./pages/AboutUsPage/LogoDescSection.jsX";
 
 function AppRouter() {
     return (
@@ -17,6 +18,7 @@ function AppRouter() {
                     <Route path="products" element={<ProductPage />} />
                     <Route path="about" element={<AboutUsPage />} />
                     <Route path="contact" element={<ContactPage />} />
+                    <Route path="/about/logo-desc" element={<LogoDescSection />} />
                     <Route path="*" element={<NotFoundPage />} />
                 </Route>
             </Routes>
